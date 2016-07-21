bbd52c25326bf69117953840b1cd7a64af756346
VVN <apple@appledemacbook-pro.local>
2016-07-21 13:37:16 +0800

    Initial Commit

---
+++ MyAppQD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAppQD
+//
+//  Created by VVN on 2016/7/21.
+//  Copyright © 2016年 VVN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
